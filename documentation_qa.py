51e172eab73fed554fcb7ed81c39dca303bf1b43
Tuana Celik <tuana.celik@deepset.ai>
2023-08-10 12:20:45 -0700

    fixes

--- documentation_qa.py
+++ documentation_qa.py
@@ -1,14 +1,15 @@
 import os
 from dotenv import load_dotenv
-from index_files import index_to_document_store
 from haystack import Pipeline
-from haystack.nodes import PromptNode
+from haystack.nodes import EmbeddingRetriever, PromptNode
+from milvus_haystack import MilvusDocumentStore
 
 load_dotenv()
 OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
 
-document_store, retriever = index_to_document_store()
+document_store = MilvusDocumentStore()
 
+retriever = EmbeddingRetriever(document_store=document_store, embedding_model="sentence-transformers/multi-qa-mpnet-base-dot-v1")
 prompt_node = PromptNode(model_name_or_path="gpt-4", api_key=OPENAI_API_KEY, default_prompt_template="deepset/question-answering")
 
 query_pipeline = Pipeline()
@@ -16,5 +17,3 @@ query_pipeline.add_node(component=retriever, name="retriever", inputs=["Query"])
 query_pipeline.add_node(component=prompt_node, name="prompt_node", inputs=["retriever"])
 
 result = query_pipeline.run(query="How do I install milvus?")
-# def query(query):
-#     query_pipeline.run(query=query)
--- index_files.py
+++ index_files.py
@@ -1,24 +1,23 @@
 from haystack import Pipeline
-from haystack.nodes import Crawler, PreProcessor, BM25Retriever
+from haystack.nodes import Crawler, PreProcessor, EmbeddingRetriever
 from milvus_haystack import MilvusDocumentStore
 
-def index_to_document_store():
-    document_store = MilvusDocumentStore(recreate_index=True, return_embedding=True, similarity="cosine")
-    crawler = Crawler(urls=["https://milvus.io/docs/"], crawler_depth=1, overwrite_existing_files=True, output_dir="crawled_files")
-    preprocessor = PreProcessor(
-        clean_empty_lines=True,
-        clean_whitespace=False,
-        clean_header_footer=True,
-        split_by="word",
-        split_length=500,
-        split_respect_sentence_boundary=True,
-    )
-    
-    indexing_pipeline = Pipeline()
-    indexing_pipeline.add_node(component=crawler, name="crawler", inputs=['File'])
-    indexing_pipeline.add_node(component=preprocessor, name="preprocessor", inputs=['crawler'])
-    indexing_pipeline.add_node(component=document_store, name="document_store", inputs=['retriever'])
+document_store = MilvusDocumentStore(recreate_index=True, return_embedding=True, similarity="cosine")
+crawler = Crawler(urls=["https://milvus.io/docs/"], crawler_depth=1, overwrite_existing_files=True, output_dir="crawled_files")
+preprocessor = PreProcessor(
+    clean_empty_lines=True,
+    clean_whitespace=False,
+    clean_header_footer=True,
+    split_by="word",
+    split_length=500,
+    split_respect_sentence_boundary=True,
+)
+retriever = EmbeddingRetriever(document_store=document_store, embedding_model="sentence-transformers/multi-qa-mpnet-base-dot-v1")
 
-    indexing_pipeline.run()
+indexing_pipeline = Pipeline()
+indexing_pipeline.add_node(component=crawler, name="crawler", inputs=['File'])
+indexing_pipeline.add_node(component=preprocessor, name="preprocessor", inputs=['crawler'])
+indexing_pipeline.add_node(component=retriever, name="retriever", inputs=['preprocessor'])
+indexing_pipeline.add_node(component=document_store, name="document_store", inputs=['retriever'])
 
-    return document_store
+indexing_pipeline.run()
